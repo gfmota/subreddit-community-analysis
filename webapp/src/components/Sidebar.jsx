1a9fa37e1a5f92656a0be706d7a6ebe534fb99fe
gfmota <gabrielfmota134@gmail.com>
2026-08-30 12:04:47 -0300

    fix: use scroller on side bar date selector
    Signed-off-by: gfmota <gabrielfmota134@gmail.com>

--- webapp/src/components/Sidebar.jsx
+++ webapp/src/components/Sidebar.jsx
@@ -27,7 +27,7 @@ export default function Sidebar({ filters, children }) {
         {children}
       </div>
       {filters}
-      <DateSelector />
+      <DateSelector scroller />
     </div>
   );
 }
